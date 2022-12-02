bbcdedfef249984ece4ed18699aded5a83ea085f
Rafael Malta <rafa_rsbr@hotmail.com>
2022-12-02 10:40:27 -0300

    Padrão Repository para Categorias

---
+++ Lanches/Repositories/CategoriaRepository.cs
@@ -0,0 +1,21 @@
+﻿using Lanches.Context;
+using Lanches.Models;
+using Lanches.Repositories.Interfaces;
+
+namespace Lanches.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        // injeção de dependência
+        private readonly AppDbContext _context;
+
+        // injeta no construtor uma instância e atribui um contexto
+        public CategoriaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // usa a instância do contexto para acessar a tabela 'Categorias' e retorna uma lista da mesma
+        public IEnumerable<Categoria> Categorias => _context.Categorias;
+    }
+}
---
+++ Lanches/Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,10 @@
+﻿using Lanches.Models;
+
+namespace Lanches.Repositories.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        // lista de 'Categorias' somente como leitura de forma ordenada
+        IEnumerable<Categoria> Categorias { get; }
+    }
+}
