bde09f9e9f4a93d233d65936ac52514b32534890
Rafael Malta <rafa_rsbr@hotmail.com>
2022-12-02 09:28:47 -0300

    Add dados na tabela Categorias

---
+++ Lanches/Migrations/20221202121753_PopularCategorias.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lanches.Migrations
+{
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao)" + 
+                "VALUES('Normal', 'Lache feito com ingredientes normais')");
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao)" +
+                "VALUES('Natural', 'Lache feito com ingredientes integrais e naturais')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
