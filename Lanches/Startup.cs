1d3167ef7c2b9413c36724c6a20bcb0f8ed40bcd
Rafael Malta <rafa_rsbr@hotmail.com>
2022-12-03 15:11:02 -0300

    Serviço dos repositórios: Injeção de Dependência

--- Lanches/Startup.cs
+++ Lanches/Startup.cs
@@ -1,4 +1,6 @@
 ﻿using Lanches.Context;
+using Lanches.Repositories;
+using Lanches.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Lanches;
@@ -16,6 +18,9 @@ public class Startup
     {
         services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddTransient<ILancheRepository, LancheRepository>();
+        services.AddTransient<ICategoriaRepository, CategoriaRepository>();
 
         services.AddControllersWithViews();
     }
